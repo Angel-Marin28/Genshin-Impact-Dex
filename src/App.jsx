29eb791ad713b58a004ebc59699c6f7fbdd8f31b
Angel Garcia Marin <isc18350309@gmail.com>
2022-05-25 11:30:59 -0500

    Creando botón desplegable y enlazando con la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+const opciones = async () =>{
+  return await fetch ("https://api.genshin.dev/")
+}
+console.log(opciones());
 
 const App  = () => {
   return (
-    <div className="App">
+    <div className="container">
      <h1>Genshin Impact Dex</h1>
+     <hr/>
+     <select>
+       <option value=""> Seleccione el tipo de información</option>
+       <option value="artifacts"> Artefactos</option>
+       <option value="boss"> Jefes</option>
+       <option value="characters"> Personajes</option>
+       
+     </select>
     </div>
   );
 }
